fc71e1f05da38f8bc1edf9aefddf2324b38d1405
Phillip Burleigh <pburle2@illinois.edu>
2022-06-16 22:45:38 -0600

    Implement basic player list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class CalculatorApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      text: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) return;
+
+    const newPlayer = {
+      text: this.state.text,
+      id: Date.now(),
+    };
+
+    this.setState((state) => ({
+      players: state.players.concat(newPlayer),
+      text: "",
+    }));
+  }
+
+  handleRemove(id) {
+    this.setState({
+      players: this.state.players.filter((player) => player.id !== id),
+    });
+  }
+
+  render() {
+    const players = this.state.players;
+
+    const list = players.map((player) => (
+      <li key={player.id}>
+        {player.text}
+        {"\t"}
+        <button onClick={() => this.handleRemove(player.id)}>X</button>
+      </li>
+    ));
+
+    return (
+      <div>
+        <h3>Player List</h3>
+        <ol>{list}</ol>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-player">Player Name: </label>
+          <input
+            id="new-player"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>Add Player</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<CalculatorApp />);
