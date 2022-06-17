466032611ea116fc3af5580e0aa37c8ce5897d29
Phillip Burleigh <pburle2@illinois.edu>
2022-06-17 12:29:12 -0600

    Add basic scoreboard and current game indicator

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ class CalculatorApp extends React.Component {
     this.state = {
       players: [],
       text: "",
+      currentGame: null,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -24,6 +25,7 @@ class CalculatorApp extends React.Component {
     const newPlayer = {
       text: this.state.text,
       id: Date.now(),
+      score: 0,
     };
 
     this.setState((state) => ({
@@ -38,8 +40,37 @@ class CalculatorApp extends React.Component {
     });
   }
 
+  changeGame(gameName) {
+    this.setState({
+      currentGame: gameName,
+    });
+  }
+
+  updateScore(id, amount) {
+    const isMatchingId = (player) => player.id === id;
+    const playerIndex = this.state.players.findIndex(isMatchingId);
+    const newPlayer = this.state.players[playerIndex];
+    newPlayer.score += amount;
+
+    const updatedPlayers = this.state.players.map((player, index) => {
+      return index === playerIndex ? newPlayer : player;
+    });
+
+    this.setState({
+      players: updatedPlayers,
+    });
+  }
+
   render() {
     const players = this.state.players;
+    const currentGame = this.state.currentGame
+      ? this.state.currentGame
+      : "No game selected";
+    const gameList = [
+      { name: "Oh Hell" },
+      { name: "Spades" },
+      { name: "Hearts" },
+    ];
 
     return (
       <div>
@@ -58,7 +89,16 @@ class CalculatorApp extends React.Component {
           <button>Add Player</button>
         </form>
         <h3>Game List</h3>
-        <GameList />
+        <GameList
+          gameList={gameList}
+          changeGame={(newGame) => this.changeGame(newGame)}
+        />
+        <h3>Current Game</h3>
+        <h4>{currentGame}</h4>
+        <Scoreboard
+          players={players}
+          updateScore={(id, amount) => this.updateScore(id, amount)}
+        />
       </div>
     );
   }
@@ -73,7 +113,7 @@ class PlayerList extends React.Component {
         {player.text}
         {"\t"}
         <button
-          className="remove-button"
+          className="round-button"
           onClick={() => this.props.handleRemove(player.id)}
         >
           X
@@ -86,13 +126,62 @@ class PlayerList extends React.Component {
 }
 
 class GameList extends React.Component {
+  handleClick(gameName) {
+    this.props.changeGame(gameName);
+  }
+
   render() {
-    const gameOhHell = "Oh Hell";
-    return (
-      <div>
-        <button onClick={() => console.log("Clicked " + gameOhHell)}>
-          {gameOhHell}
+    const gameList = this.props.gameList.map((game) => (
+      <button key={game.name} onClick={() => this.handleClick(game.name)}>
+        {game.name}
+      </button>
+    ));
+    return <div>{gameList}</div>;
+  }
+}
+
+class GameOhHell extends React.Component {}
+
+class Scoreboard extends React.Component {
+  render() {
+    const players = this.props.players;
+
+    const playerList = players.map((player) => (
+      <div key={player.id} className="grid-item">
+        {player.text}
+      </div>
+    ));
+
+    const scoreList = players.map((player) => (
+      <div key={player.id} className="grid-item">
+        <button
+          className="round-button"
+          onClick={() => this.props.updateScore(player.id, -1)}
+        >
+          -1
         </button>
+        {"\t"}
+        {player.score}
+        {"\t"}
+        <button
+          className="round-button"
+          onClick={() => this.props.updateScore(player.id, 1)}
+        >
+          +1
+        </button>
+      </div>
+    ));
+
+    return (
+      <div className="grid-container">
+        <div>
+          <div className="grid-item grid-header">Player</div>
+          {playerList}
+        </div>
+        <div>
+          <div className="grid-item grid-header">Score</div>
+          {scoreList}
+        </div>
       </div>
     );
   }
